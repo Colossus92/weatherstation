6340128ef630a8f8df3701f2a3b11ccb0852d23a
Calvin Richardson <calvin.richardson@sogeti.com>
2019-06-09 00:15:56 +0200

    *Added JSONDoc to SensorDataEntity

--- src/main/java/com/devtestguild/weatherstation/Sensordata/SensorDataEntity.java
+++ src/main/java/com/devtestguild/weatherstation/Sensordata/SensorDataEntity.java
@@ -1,6 +1,7 @@
 package com.devtestguild.weatherstation.Sensordata;
 
 import org.jsondoc.core.annotation.ApiObject;
+import org.jsondoc.core.annotation.ApiObjectField;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -12,14 +13,19 @@ import java.util.Date;
 @Table(name="sensor_data")
 public class SensorDataEntity {
 
+    @ApiObjectField(description = "ID of sensordata entity")
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
 
+    @ApiObjectField(description = "Recorded value of sensordata")
     private String value;
 
+    @ApiObjectField(description = "Type of datarecord, like temperature")
     @Column(name="data_type")
     private String typeOfData;
+
+    @ApiObjectField(description = "ID of sensor made record")
     private int sensorId;
 
     public SensorDataEntity(){
