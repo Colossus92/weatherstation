3dd41fbc8930abddf73516a7b1c8d26022ffee3b
Calvin Richardson <calvin.richardson@sogeti.com>
2019-06-26 20:07:37 +0200

    Enable Cors

--- src/main/java/com/devtestguild/weatherstation/WeatherstationApplication.java
+++ src/main/java/com/devtestguild/weatherstation/WeatherstationApplication.java
@@ -3,6 +3,8 @@ package com.devtestguild.weatherstation;
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJSONDoc
@@ -10,6 +12,15 @@ public class WeatherstationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WeatherstationApplication.class, args);
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
