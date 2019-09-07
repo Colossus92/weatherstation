aa668efccd50f3ec12ce3e510da451da252eddd3
Calvin Richardson <calvin.richardson@sogeti.com>
2019-09-07 21:44:26 +0200

    New trial to add CorsConfigurer

--- src/main/java/com/devtestguild/weatherstation/WeatherstationApplication.java
+++ src/main/java/com/devtestguild/weatherstation/WeatherstationApplication.java
@@ -3,8 +3,10 @@ package com.devtestguild.weatherstation;
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJSONDoc
@@ -14,13 +16,13 @@ public class WeatherstationApplication {
         SpringApplication.run(WeatherstationApplication.class, args);
     }
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
-
 }
