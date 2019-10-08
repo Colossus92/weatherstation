398e4d256295e423ef8266e34cc229d557b502dd
Calvin Richardson <calvin.richardson@sogeti.com>
2019-10-08 22:39:23 +0200

    Remove javax.xml.ws import

--- src/main/java/com/devtestguild/weatherstation/Sensor/SensorService.java
+++ src/main/java/com/devtestguild/weatherstation/Sensor/SensorService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.xml.ws.Response;
 import java.util.List;
 
 @Service
